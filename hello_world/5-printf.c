488154ec1a5c79b69bf58255e0488589cf723789
Darvid <darvid.torres@gmail.com>
2023-02-17 17:33:35 -0800

    Improving betty style

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -4,8 +4,8 @@
 *Descrption: the print the text
 *Return: zero
 */
-int main(void) 
+int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
+pintf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
