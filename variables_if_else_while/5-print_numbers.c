fe863436d03298696cf81267635ab97befc7fb4d
Darvid <darvid.torres@gmail.com>
2023-02-21 12:54:39 -0800

    using while to print digits

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+*main - function main
+*Description: print single digits
+*Return: 0
+*/
+int main(void)
+{
+int n = 0;
+while (n < 10)
+{
+printf("%i", n);
+n++;
+}
+printf("\n");
+return (0);
+}
