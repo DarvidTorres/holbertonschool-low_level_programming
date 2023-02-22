81d6065e64ba108a6f2a088b531cbdf09572641c
Darvid <darvid.torres@gmail.com>
2023-02-22 13:49:54 -0800

    print _putchar calling a header file in directory

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+/**
+*main - function main entry point
+*Description: print '_putchar'
+*return 0
+*/
+#include <stdio.h>
+#include "main.h"
+int main(void)
+{
+printf("_putchar\n");
+return(0);
+}
