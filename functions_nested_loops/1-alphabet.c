630fed0d8932494d479610c3870f4ee7cdec0f0f
Darvid <darvid.torres@gmail.com>
2023-02-22 14:47:10 -0800

    print lowercase alphabet without using standard library

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+*main - main function entry point
+*Description: print lowrcase alphabet
+*Return: 0
+*/
+int main(void)
+{
+char i = 'a';
+while (i <= 'z')
+{
+_putchar(i);
+}
+i++;
+ _putchar('\n');
+}
