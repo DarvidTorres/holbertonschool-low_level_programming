a6ec240aab8d39e9d5e66055b47420e52b9b501d
Darvid <darvid.torres@gmail.com>
2023-02-22 17:23:22 -0800

    get absolute value using given function

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+*_abs - function given
+*@n: variable
+*Return: 0
+*/
+int _abs(int n)
+{
+if (n < 0)
+{
+n = n * -1;
+return (n);
+}
+else
+{
+return (n);
+}
+}
