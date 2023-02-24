7a30bfdc603b7d8a40642e56091200bba97c6469
Darvid <darvid.torres@gmail.com>
2023-02-24 09:42:19 -0800

    using an if loop

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+*_isupper - check uppercase
+*Description: use isupper defined function
+*@c: variabe
+*Return: 0
+*/
+int _isupper(int c)
+{
+char c;
+if (_isupper(c) == 1)
+{
+_putchar("%d", 1);
+}
+else
+{
+_putchar("%d",0);
+}
+}
---
+++ more_functions_nested_loops/main.h
@@ -0,0 +1,4 @@
+#ifndef HEADER_FILE_NAME
+#define HEADER_FILE_NAME
+int _isupper(int c);
+#endif
