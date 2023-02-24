e37cd57f0c88dfce01f615b7ac70c38d425c899b
Darvid <darvid.torres@gmail.com>
2023-02-24 11:20:06 -0800

    first try

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+*more_numbers - print numbers from 0 14 10x
+*Description: function that prints 0 to 14 10x
+*Return: 0
+*/
+void more_numbers(void)
+{
+int i, k = 0;
+while (k < 10)
+{
+for (i = 0; i < 15; i++)
+{
+_putchar(i);
+_putchar('\n');
+}
+k++;
+}
+}
