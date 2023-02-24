50bdfdfcf22715ae54d5f829481e41bfeab44efb
Darvid <darvid.torres@gmail.com>
2023-02-24 12:28:40 -0800

    asdf

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+*print_line - print lines depending on input
+*@n: parameter
+*/
+void ptint_line(int n)
+{
+int l;
+if (n > 0)
+{
+for (l = 0; l <n; l++)
+_putchar('_');
+}
+_putchar('\n');
+}
