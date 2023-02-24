b74efe73aa79616162030b82cca74aa00d4cb7a6
Darvid <darvid.torres@gmail.com>
2023-02-24 12:34:26 -0800

    print diagonal symbols

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include"main.h"
+/**
+*print_line - function
+*@n: variable
+*/
+void print_line(int n)
+{
+int i = 1;
+while (i <= n)
+{ 
+_puthar('_');
+i++;
+_putchar('\n');
+}
