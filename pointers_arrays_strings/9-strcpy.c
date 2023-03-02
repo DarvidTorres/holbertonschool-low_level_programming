96deb2f406d21cd4416cd4c18394faa9919a819b
Darvid <darvid.torres@gmail.com>
2023-03-02 14:51:17 -0800

    function that copies string pointed

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+/**
+*_strcpy - function that copies 
+*@dest: parameter
+@src: parameter
+*Return: char
+*/
+char *_strcpy(char *dest, char *src)
+{
+int i = 0;
+int j;
+while (src[i] != '\0')
+{
+i++;
+}
+for (j = 0; j <= i; j++)
+{
+dest[j] = src[j];
+}
+return (dest);
+}
