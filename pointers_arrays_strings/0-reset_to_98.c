ce3ffc80ec80e7f9cc8f39ff511e450a239fa727
Darvid <darvid.torres@gmail.com>
2023-03-02 13:53:45 -0800

    working with pointers

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - function that resets a value
+ * @n: parameter
+ * Description: function that sets the value argument to 98
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+}
