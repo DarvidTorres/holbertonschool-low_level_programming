f448222ff6ea8d79f11df477c0baeb7fa215e476
Darvid <darvid.torres@gmail.com>
2023-03-02 14:29:25 -0800

    print string in reverse order

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+*rev_string - get all characters of a string starting from last'
+*@s: parameter
+*/
+void rev_string(char *s)
+{
+int len = 0, i = 0;
+char x;
+while (s[i++])
+len++;
+for (i = len - 1; i >= len / 2; i--)
+{
+x = s[i];
+s[i] = s[len - i - 1];
+s[len - i -1] = x;
+}
+}
