f13ff95f1162393f651fe816ffb49b1b7808dacf
Darvid <darvid.torres@gmail.com>
2023-03-03 11:51:48 -0800

    concatenate strings

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+*_strncat - function to concatenate strings
+*@dest: parameter
+*@src: parameter
+*@n: parameter
+*Return: 0;
+*/
+char *_strncat(char *dest, char *src, int n)
+{
+int i = 0, lenD = 0;
+while (dest[i++])
+lenD++;
+for (i= 0; src[i] && i < n; i++)
+dest[lenD++] = src[i];
+return (dest);
+}
