8dc826942daf7af9a1a03fd219fb4ef76d727b97
Darvid <darvid.torres@gmail.com>
2023-03-08 20:40:40 -0800

    add functions

---
+++ static_libraries/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+*_isupper - check uppercase
+*Description: define an isuper function
+*@c: parameter
+*Return: 1 or 0
+*/
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
