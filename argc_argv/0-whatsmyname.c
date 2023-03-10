c4b944e2b3eb96911ef16750fb3a53c24325b3ce
Darvid <darvid.torres@gmail.com>
2023-03-10 11:35:06 -0800

    working with arguments to main

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#incldue <stdlib.h>
+/**
+*main - function
+*@argc: argument counter
+*@argv: argument array
+*Return: 0
+*/
+int main (int argc, char **argv)
+{
+for (argc > 0)
+{
+printf("%s\n", argv[0]);
+}
+return (0);
+}
