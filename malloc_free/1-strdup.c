bf9de47cfb000c1fa6e51a0a2f3e5d143ab06a8a
Darvid <darvid.torres@gmail.com>
2023-03-15 20:34:47 -0700

    function that returns a pointer to a new string

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+*_strdup - returns a pointer to a new string
+*@str: char *
+*REturn: char *
+*/
+char *_strdup(char *str)
+{
+int len;
+int i;
+char *arr = NULL;
+len = 0;
+if (str == 0)
+{
+return (NULL);
+}
+while (str[len] != '\0')
+{
+len++;
+}
+arr = malloc(len + 1);
+if (arr == 0)
+{
+return (NULL);
+}
+i = 0;
+while (i < len)
+{
+arr[i] = str[i];
+i++;
+}
+return (arr);
+}
