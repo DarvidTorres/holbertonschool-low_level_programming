82f92b94c261ab6200ea2fe77e1a57f7710b6a6b
darvid <darvid.torres@gmail.com>
2023-03-26 16:05:54 -0600

    initialize a structure

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * *init_dog - function
+ * @d: parameter
+ * @name: parameter
+ * @age: parameter
+ * @owner: parameter
+ * *Return: 0
+ * */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+if (d)
+{
+d -> name = name;
+d -> age = age;
+d -> owner = owner;
+}
+}
