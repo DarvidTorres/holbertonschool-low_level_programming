4434c1145dbb71a63dcf489144f4bab049cd280e
darvid <darvid.torres@gmail.com>
2023-03-26 15:54:36 -0600

    Structures

---
+++ structures_typedef/dog.h
@@ -0,0 +1,22 @@
+#ifndef DOG_H
+#define DOG_H
+#include <stdio.h>
+#include <stdlib.h>
+/**
+**struct dog - printf
+*@name:parameter
+*@age: parameter
+*@owner: parameter
+*Description: define structure
+*/
+struct dog
+{
+char * name;
+float age;
+char *owner;
+};
+typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+#endif
