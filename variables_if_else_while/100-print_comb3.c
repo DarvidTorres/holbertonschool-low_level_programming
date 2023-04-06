cd9e0fffdd44430584ca1e27bed2afa4b9abcd70
darvid <darvid.torres@gmail.com>
2023-04-06 10:21:17 -0600

    betty style

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -10,16 +10,16 @@ int i;
 int j;
 for (i = '0'; i < '9'; i++)
 {
-	for (j = i + 1; j <= '9'; j++)
-	{
-	    putchar(i);
-	    putchar(j);
-	    if (i != '8' || j != '9')
-        {
-        putchar(',');
-        putchar(' ');
-        }
-	}
+for (j = i + 1; j <= '9'; j++)
+{
+putchar(i);
+putchar(j);
+if (i != '8' || j != '9')
+{
+putchar(',');
+putchar(' ');
+}
+}
 }
 putchar('\n');
 return (0);
