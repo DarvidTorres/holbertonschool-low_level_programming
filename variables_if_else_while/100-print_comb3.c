f21a2bb844bbfeea0f3b66cc2335616047aef5ef
darvid <darvid.torres@gmail.com>
2023-04-06 10:20:26 -0600

    adding condition

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -6,19 +6,21 @@
 */
 int main(void)
 {
-	int i;
-	int j;
-
-	for (i = '0'; i < '9'; i++)
+int i;
+int j;
+for (i = '0'; i < '9'; i++)
+{
+	for (j = i + 1; j <= '9'; j++)
 	{
-		for (j = i + 1; j <= '9'; j++)
-		{
-			putchar(i);
-			putchar(j);
-			putchar(',');
-            putchar(' ');
-		}
+	    putchar(i);
+	    putchar(j);
+	    if (i != '8' || j != '9')
+        {
+        putchar(',');
+        putchar(' ');
+        }
 	}
-	putchar('\n');
-	return (0);
+}
+putchar('\n');
+return (0);
 }
