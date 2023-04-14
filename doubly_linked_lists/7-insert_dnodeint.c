ff6df8a56074d872957c8c68dbb5cb41f47cb91e
darvid <darvid.torres@gmail.com>
2023-04-14 17:06:28 -0600

    a different approach

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -1,41 +1,43 @@
 #include "lists.h"
 /**
-* insert_dnodeint_at_index - inserts a new node at a given position
-* @h: dlistint_t
-* @idx: unsigned int
-* @n: int
-* Return: new
+* *insert_dnodeint_at_index - function add node at index
+* @h: variable struct
+* @idx: variable int
+* @n: variable int
+* Return: struct
 */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-dlistint_t *cop = *h, *new;
-unsigned int node;
+dlistint_t *new, *aux = *h;
+unsigned int count = 0;
+if (!h || (!*h && idx != 0))
+return (NULL);
+if (idx == 0)
+return (add_dnodeint(h, n));
 new = malloc(sizeof(dlistint_t));
-if (new == NULL)
+if (!new)
 return (NULL);
 new->n = n;
-new->prev = NULL;
-if (idx == 0)
+while (aux->next || count == idx)
 {
-new->next = cop;
-*h = new;
+if (count == idx)
+{
+aux->prev->next = new;
+new->prev = aux->prev;
+new->next = aux;
+aux->prev = new;
 return (new);
 }
-for (node = 0; node < (idx - 1); node++)
+count++;
+aux = aux->next;
+}
+if (count + 1 == idx)
 {
-if (cop == NULL || cop->next == NULL)
+new->next = NULL;
+aux->next = new;
+new->prev = aux;
+return (new);
+}
+free(new);
 return (NULL);
-cop = cop->next;
-}
-new->next = cop->next;
-if (cop != NULL)
-{
-while (cop->prev != NULL)
-cop = cop->prev;
-}
-cop->next = new;
-if (cop != NULL)
-cop->prev = new;
-*h = new;
-return (new);
 }
