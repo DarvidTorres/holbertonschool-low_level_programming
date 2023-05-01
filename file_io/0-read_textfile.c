a84e57cee7529f5c7f0bd49a67a68837b1689555
Darvid <darvid.torres@gmail.com>
2023-05-01 16:50:55 -0600

    adjust code

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -7,25 +7,19 @@
 */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-char *buff;
-int fd, read_file, write_file;
-if (!filename)
-return (0);
-fd = open(filename, O_RDONLY);
-if (fd == -1)
-return (0);
-buff = malloc(sizeof(char) * letters + 1);
+	int rt, fr, wr;
+	char *c;
 
-if (!buff)
-return (0);
-read_file = read(fd, buff, letters);
-if (read_file == -1)
-return (0);
-buff[letters] = '\0';
-write_file = write(STDOUT_FILENO, buff, read_file);
-if (write_file == -1)
-return (0);
-free(buff);
-close(fd);
-return (write_file);
+	if (filename == NULL)
+		return (0);
+	rt = open(filename, O_RDONLY);
+	if (rt == -1)
+		return (0);
+	c = malloc(sizeof(char) * (letters));
+	if (c == NULL)
+		return (0);
+	fr = read(rt, c, letters);
+	wr = write(STDOUT_FILENO, c, fr);
+	close(rt);
+	return (wr);
 }
